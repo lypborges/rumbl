7662c286b36c1c74ecc9319403f0c408c56f393b
Felipe Borges Ferreira <lypborges@gmail.com>
2016-08-15 10:01:33 -0300

    require user login

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,9 +1,9 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  plug :authenticate when action in [:index,:show]
 
   def index(conn, _params) do
     users = Repo.all(Rumbl.User)
-
     render conn, "index.html", users: users
   end
 
@@ -31,6 +31,18 @@ defmodule Rumbl.UserController do
       |> redirect(to: user_path(conn, :index))
     {:error, changeset} ->
       render(conn, "new.html", changeset: changeset)
+    end
+
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
     end
 
   end
